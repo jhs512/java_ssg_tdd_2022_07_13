6cc57f464671c28ab69a9dab13279bfe67cbb4ae
jhs512 <jangka512@gmail.com>
2022-07-13 13:47:44 +0900

    AppTestRunner 도입하여 중복소스코드 제거

--- src/test/java/com/ll/exam/AppTest.java
+++ src/test/java/com/ll/exam/AppTest.java
@@ -11,18 +11,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class AppTest {
     @Test
     public void 등록을_하면_명언과_작가를_물어본다() {
-        Scanner sc = TestUtil.genScanner("""
+        String rs = AppTestRunner.run("""
                 등록
                 나의 죽음을 적들에게 알리지 말라
                 이순신
                 종료
                 """);
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-
-        new App(sc).run();
-
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
 
         assertTrue(rs.contains("명언 : "));
         assertTrue(rs.contains("작가 : "));
@@ -30,13 +24,9 @@ public class AppTest {
 
     @Test
     public void 프로그램_시작시_타이틀_출력_그리고_종료() {
-        Scanner sc = TestUtil.genScanner("종료");
-        ByteArrayOutputStream output = TestUtil.setOutToByteArray();
-
-        new App(sc).run();
-
-        String rs = output.toString();
-        TestUtil.clearSetOutToByteArray(output);
+        String rs = AppTestRunner.run("""
+                종료
+                """);
 
         assertTrue(rs.contains("== 명언 SSG =="));
         assertTrue(rs.contains("명령)"));
