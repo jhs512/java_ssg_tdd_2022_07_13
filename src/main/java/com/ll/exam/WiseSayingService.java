68d4e4e6ff517779a72f15116efa3d3ce797c62f
jhs512 <jangka512@gmail.com>
2022-07-13 20:39:37 +0900

    리포지터리 도입

--- src/main/java/com/ll/exam/WiseSayingService.java
+++ src/main/java/com/ll/exam/WiseSayingService.java
@@ -1,65 +1,32 @@
 package com.ll.exam;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class WiseSayingService {
-    private int wiseSayingsLastId;
-    private List<WiseSaying> wiseSayings;
+
+    private WiseSayingRepository wiseSayingRepository;
 
     public WiseSayingService() {
-        wiseSayingsLastId = 0;
-        wiseSayings = new ArrayList<>();
+        wiseSayingRepository = new WiseSayingRepository();
     }
 
     public WiseSaying write(String content, String author) {
-        int id = ++wiseSayingsLastId;
-
-        WiseSaying wiseSaying = new WiseSaying(id, content, author);
-
-        wiseSayings.add(wiseSaying);
-
-        System.out.printf("%d번 명언이 등록되었습니다.\n", id);
-
-        return wiseSaying;
+        return wiseSayingRepository.write(content, author);
     }
 
     public List<WiseSaying> findAll() {
-        return wiseSayings;
+        return wiseSayingRepository.findAll();
     }
 
     public WiseSaying findById(int id) {
-        for (WiseSaying wiseSaying : wiseSayings) {
-            if (wiseSaying.id == id) {
-                return wiseSaying;
-            }
-        }
-
-        return null;
+        return wiseSayingRepository.findById(id);
     }
 
     public boolean modify(int id, String content, String author) {
-        WiseSaying wiseSaying = findById(id);
-
-        if (wiseSaying == null) {
-            return false;
-        }
-
-        wiseSaying.content = content;
-        wiseSaying.author = author;
-
-        return true;
+        return wiseSayingRepository.modify(id, content, author);
     }
 
     public boolean remove(int id) {
-        WiseSaying wiseSaying = findById(id);
-
-        if (wiseSaying == null) {
-            return false;
-        }
-
-        wiseSayings.remove(wiseSaying);
-
-        return true;
+        return wiseSayingRepository.remove(id);
     }
 }
