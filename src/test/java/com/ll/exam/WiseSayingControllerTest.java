d76efb627288f247100654b999132ec075e415b2
jhs512 <jangka512@gmail.com>
2022-07-15 23:38:33 +0900

    AppTestRunner 에서 자동으로 종료 명령을 끝에 붙여줌

--- src/test/java/com/ll/exam/WiseSayingControllerTest.java
+++ src/test/java/com/ll/exam/WiseSayingControllerTest.java
@@ -26,7 +26,6 @@ public class WiseSayingControllerTest {
                 등록
                 나의 죽음을 적들에게 알리지 말라
                 이순신
-                종료
                 """);
 
         assertTrue(rs.contains("명언 : "));
@@ -42,7 +41,6 @@ public class WiseSayingControllerTest {
                 등록
                 나에게 불가능이란 없다.
                 나폴레옹
-                종료
                 """);
 
         assertTrue(rs.contains("1번 명언이 등록되었습니다."));
@@ -59,7 +57,6 @@ public class WiseSayingControllerTest {
                 나에게 불가능이란 없다.
                 나폴레옹
                 목록
-                종료
                 """);
 
         assertTrue(rs.contains("번호 / 작가 / 명언"));
@@ -82,7 +79,6 @@ public class WiseSayingControllerTest {
                 나의 죽음을 적들에게 알리지 마라!
                 이순신장군
                 목록
-                종료
                 """);
 
         assertTrue(rs.contains("1 / 이순신 / 나의 죽음을 적들에게 알리지 말라"));
@@ -101,7 +97,6 @@ public class WiseSayingControllerTest {
                 삭제?id=1
                 목록
                 삭제?id=1
-                종료
                 """);
 
         assertTrue(rs.contains("1번 명언이 삭제되었습니다."));
@@ -112,9 +107,7 @@ public class WiseSayingControllerTest {
 
     @Test
     public void 프로그램_시작시_타이틀_출력_그리고_종료() {
-        String rs = AppTestRunner.run("""
-                종료
-                """);
+        String rs = AppTestRunner.run("");
 
         assertTrue(rs.contains("== 명언 SSG =="));
         assertTrue(rs.contains("명령)"));
